233b6f200b73247ef2b11d957d95510cef603338
Chris Chi <ccchi@midway3-login1.rcc.local>
2022-07-19 14:04:06 -0500

    switch from multiprocessing to mpi4py

--- periodic_orbits.py
+++ periodic_orbits.py
@@ -19,7 +19,7 @@ def find_limit_cycle(q, period_rtol=1e-5):
         mask = mask * (cA > 0)
     else:
         warnings.warn("Concentrations too low")
-        return np.full_like(y0, np.nan), np.nan
+        return np.concatenate([np.full_like(y0, np.nan), np.array([np.nan])])
 
     ind = np.arange(mask.shape[0])[mask][:2]
     y0 = y0.at[ind].add(np.array([-1e-2, 1e-2]))
@@ -37,20 +37,24 @@ def find_limit_cycle(q, period_rtol=1e-5):
         int0 = scipy.integrate.solve_ivp(model.f, jac=model.jac, t_span=(0, 20 * tau0), y0=y0, t_eval=np.linspace(12 * tau0, 20 * tau0, int(1 / period_rtol)), method="LSODA")
     except:
         warnings.warn("Integration failed")
-        return np.full_like(y0, np.nan), np.nan
+        return np.concatenate([np.full_like(y0, np.nan), np.array([np.nan])])
+
+    if not int0.success:
+        warnings.warn("Integration failed")
+        return np.concatenate([np.full_like(y0, np.nan), np.array([np.nan])])
     
     if np.linalg.norm(model.f(0, int0.y[:, -1])) < 1e-9:
         warnings.warn("Converged to fixed point")
-        return int0.y[:, -1], np.inf
+        return np.concatenate([int0.y[:, -1], np.array([np.inf])])
    
     a_whiten = (int0.y[-1, :] - int0.y[-1, :].mean()) / np.std(int0.y[-1, :])
     autocorr_a = scipy.signal.correlate(a_whiten, a_whiten) / int0.t.shape[0]
     autocorr_peaks = scipy.signal.find_peaks(autocorr_a, prominence=0.3)
 
-    if autocorr_peaks[0].shape < 5:
+    if autocorr_peaks[0].shape[0] < 5:
         warnings.warn("Failed to converge")
-        return np.full_like(y0, np.nan), np.nan
+        return np.concatenate([np.full_like(y0, np.nan), np.array([np.nan])])
 
     tau1 = np.median(autocorr_peaks[0][1:] - autocorr_peaks[0][:-1]) * (int0.t[-1] - int0.t[-2])
 
-    return int0.y[:, -1], tau1
+    return np.concatenate([int0.y[:, -1], np.array([tau1])])
