c2a7c1ca65a7e5cf4315a7d4a70af96152ec406a
Chris Chi <ccchi@midway3-login2.rcc.local>
2022-07-26 18:59:23 -0500

    compute measure for asymmetry of speed through cycle

--- KaiA_continuation.py
+++ KaiA_continuation.py
@@ -17,14 +17,14 @@ jac_arclength = jax.jit(jax.jacfwd(f_arclength, argnums = 1))
 def arclength(rates, y0):
 
     if np.any(np.isnan(rates)) or np.any(np.isnan(y0)):
-        return np.nan
+        return np.nan, np.nan
 
     model = KaiODE(rates)
     result = scipy.integrate.solve_ivp(f_arclength, jac=jac_arclength, t_span=(0, 1), y0=y0, args=(model,), t_eval=np.linspace(0, 1, 1000), method="LSODA", atol=1e-9, rtol=1e-6)
 
     if result.success:
-        ydot = jax.lax.scan(lambda _, y:(_, f_arclength(0, y, model), 0, result.y.T)
-        speed = ydot[-1, :]
+        ydot = jax.lax.scan(lambda _, y:(_, f_arclength(0, y, model)), 0, result.y.T)
+        speed = np.linalg.norm(ydot[1][:-1, :], axis=1)
         speed_peaks = scipy.signal.find_peaks(numpy.array(speed), width=0, height=float(np.quantile(speed, 0.99) / 4))
         return result.y[-1, -1], (result.t[1] - result.t[0]) * speed_peaks[1]["widths"].sum()
     else:
@@ -36,7 +36,8 @@ parser.add_argument("-o", type=str, required=True)
 args = parser.parse_args()
 
 indata = np.load(args.i)
-out = numpy.zeros((indata.shape[0], 7))
+out = numpy.zeros((indata.shape[0], 5))
+#out = numpy.zeros((indata.shape[0], 1))
 partial = numpy.zeros_like(out)
 
 comm = MPI.COMM_WORLD
@@ -55,7 +56,7 @@ while i < ntasks:
     partial[i, 1] = KaiA1en1[-1]
     partial[i, 2] = KaiA1e0[-1]
 
-    partial[i, np.array([3, 6])] = arclength(indata[i, :50], indata[i, 50:])
+    #partial[i] = arclength(indata[i, :50], indata[i, 50:])[1]
     partial[i, 4] = arclength(indata[i, :50], KaiA1en2[:-1])[0]
     partial[i, 5] = arclength(indata[i, :50], KaiA1en1[:-1])[0]
 
