075ce4a1cb499c4128b294d3c30284ee3979fd91
Chris Chi <ccchi@midway3-login2.rcc.local>
2022-08-04 03:18:29 -0500

    provide correct value of ya in shooting

--- periodic_orbits.py
+++ periodic_orbits.py
@@ -60,3 +60,60 @@ def find_limit_cycle(rates, y0, tau0=None, perturb=True, period_rtol=1e-5, n_per
     tau1 = np.median(autocorr_peaks[0][1:] - autocorr_peaks[0][:-1]) * (int0.t[-1] - int0.t[-2])
 
     return np.concatenate([int0.y[:, -1], np.array([tau1])])
+
+@jax.jit
+def f_M(t, y, model, tau):
+
+    return tau * np.concatenate([model.f_red(t, y[:15]), np.ravel(model.jac_red(t, y[:15])@y[15:].reshape((15, 15), order="F"), order="F")])
+
+jac_M = jax.jit(jax.jacfwd(f_M, argnums=1))
+
+def single_shooting(rates, y0, tau0, tol=1e-9, maxiter=10):
+
+    model = KaiODE(rates)
+    porbit0 = scipy.integrate.solve_ivp(model.f_red, jac=model.jac_red, t_span=(0, tau0), y0=y0, t_eval=np.linspace(0, tau0, 1000), method="LSODA", atol=1e-9, rtol=1e-6)
+
+    if not porbit0.success:
+
+        return np.concatenate([y0, np.array([tau0])])
+
+    phase_species = np.argmax(np.max(porbit.y, axis=1) - np.min(porbit.y, axis=1))
+    porbit1 = scipy.integrate.solve_ivp(model.f_red, jac=model.jac_red, t_span=(0, tau0), y0=porbit0.y[:, np.argmax(porbit0.y[phase_species, :])], method="LSODA", atol=1e-9, rtol=1e-6)
+
+    if not porbit1.success:
+
+        return np.concatenate([y0, np.array([tau0])])
+
+    err = np.linalg.norm(np.concatenate([porbit1.y[:, -1] - porbit1[:, 0], np.array([model.f_red(porbit1.y[phase_species, 0])])]))
+    i = 0
+    tau = tau0
+    ya = porbit1.y[:, 0]
+    yb = porbit1.y[:, -1]
+    err0 = np.linalg.norm(np.concatenate([yb - ya, np.array(model.f_red(0, ya)[phase_species])]))
+
+    while err > tol and i < maxiter:
+
+        M0 = numpy.zeros((ya.shape[0], ya.shape[0] + 1))
+        M0[:ya.shape[0]] = ya
+        M0[:, 1:][np.diag_indices(ya.shape[0])] = 1
+        M0 = np.ravel(M0, order="F")
+        int_M = scipy.integrate.solve_ivp(f_M, jac=jac_M, t_span=(0, 1), y0=M0, args=(tau,), method="LSODA", atol=1e-9, rtol=1e-6)
+
+        J = numpy.zeros((ya.shape[0] + 1, ya.shape[0] + 1))
+        J[:ya.shape[0], :ya.shape[0]] = int_M.y[15:, -1].reshape((15, 15), order="F")
+        J[np.diag_indices(ya.shape[0])] -= 1
+        J[:ya.shape[0], -1] = model.f_red(0, yb)
+        J[-1, :ya.shape[0]] = model.jac_red(0, ya)[phase_species, :]
+
+        resid = np.concatenate([yb - ya, np.array([model.f_red(0, ya)[phase_species]])])
+        err = np.linalg.norm(resid)
+
+        dx = np.linalg.solve(J, -resid)
+        ya += dx[:ya.shape[0]]
+        tau += dx[-1]
+
+    if i >= maxiter and err > err0:
+
+        return np.concatenate([y0, np.array([tau0])])
+
+    return np.concatenate([ya, np.array([tau])])
--- refine_orbits.py
+++ refine_orbits.py
@@ -25,8 +25,8 @@ i = comm.Get_rank()
 while i < (args.stop - args.start) // args.stride:
 
     lc = find_limit_cycle(np.exp(log_rates(traj[i, :])), np.exp(traj[i, 50:]))
-    tau1 = lc[:-1]
-    lc = single_shooting(np.exp(log_rates(traj[[i, :])), lc[:-1], np.exp(traj[i, 50:]), tau1)
+    tau1 = lc[-1]
+    lc = single_shooting(np.exp(log_rates(traj[i, :])), lc[:-1], tau1)
     partial[i, :50] = lc[-1] * np.exp(log_rates(traj[i, :]))
     partial[i, 50:] = lc[:-1]
     i += comm.Get_size()
