42d730b39b7425653941b1d517f5293ef51b45d4
Chris Chi <ccchi@midway3-login2.rcc.local>
2022-08-04 03:12:07 -0500

    single shooting

---
+++ refine_orbits.py
@@ -0,0 +1,38 @@
+from periodic_orbits import *
+from mpi4py import MPI
+import argparse
+import numpy
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", type=str, required=True)
+parser.add_argument("-start", type=int, default=0)
+parser.add_argument("-stop", type=int, default=-1)
+parser.add_argument("-stride", type=int, default=1);
+parser.add_argument("-o", type=str, required=True)
+args = parser.parse_args()
+
+traj = np.load(args.i)[args.start:args.stop:args.stride]
+
+if args.stop == -1:
+    args.stop = traj.shape[0]
+
+out = numpy.zeros(((args.stop - args.start) // args.stride, 67))
+partial = numpy.zeros_like(out)
+
+comm = MPI.COMM_WORLD
+i = comm.Get_rank()
+
+while i < (args.stop - args.start) // args.stride:
+
+    lc = find_limit_cycle(np.exp(log_rates(traj[i, :])), np.exp(traj[i, 50:]))
+    tau1 = lc[:-1]
+    lc = single_shooting(np.exp(log_rates(traj[[i, :])), lc[:-1], np.exp(traj[i, 50:]), tau1)
+    partial[i, :50] = lc[-1] * np.exp(log_rates(traj[i, :]))
+    partial[i, 50:] = lc[:-1]
+    i += comm.Get_size()
+
+comm.Allreduce([partial, MPI.DOUBLE], [out, MPI.DOUBLE], op=MPI.SUM)
+
+if comm.Get_rank() == 0:
+
+    np.save(args.o, out)
