38d359e53c56181521476e8237dbb32a83b22388
Chris Chi <ccchi@midway3-login1.rcc.local>
2022-08-20 03:51:11 -0500

    collocation class

--- collocation.py
+++ collocation.py
@@ -3,3 +3,13 @@ import scipy.sparse
 import sparsejac.sparsejac as sparsejac
 from functools import partial
 
+class colloc:
+
+    gauss_points = (1 + np.array([-0.906179845938664, -0.538469310105683, 0.0, +0.538469310105683, +0.906179845938664])) / 2
+
+    def __init__(self, model, n_mesh_point):
+
+        self.n_mesh_point = n_mesh_point
+
+    @jax.jit
+    def lagrange_poly
