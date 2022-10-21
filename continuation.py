74a26d6323dc13affa5d7dd221e1a1fffd30625f
Chris Chi <ccchi@midway3-login1.rcc.local>
2022-10-21 08:00:50 -0500

    pseudoarclength

--- continuation.py
+++ continuation.py
@@ -226,10 +226,10 @@ def compute_LL(solver1, solver2, bounds, floquet_multiplier_threshold=0.8):
     LL = 0
     LL -= 300 * (solver1.p[0] - 1)**2
     LL -= 100 * (solver2.p[0] - 1)**2
-    LL -= 1 / (100 * np.linalg.norm(solver1.f(0, solver1.y[:, 0], solver1.p, *solver1.args))**2)
-    LL -= 1 / (100 * np.linalg.norm(solver2.f(0, solver2.y[:, 0], solver2.p, *solver2.args))**2)
-    LL += LL_monodromy(solver1, floquet_multiplier_threshold)
-    LL += LL_monodromy(solver2, floquet_multiplier_threshold)
+    LL -= 1 / np.linalg.norm(solver1.y[:, 1:] - solver1.y[:, :-1], axis=0).sum()**2
+    LL -= 1 / np.linalg.norm(solver2.y[:, 1:] - solver2.y[:, :-1], axis=0).sum()**2
+    LL += LL_monodromy(solver1.y[:, 0], solver1.p[0], solver1.args[5].a0, solver1.args[5].c0, floquet_multiplier_threshold)
+    LL += LL_monodromy(solver2.y[:, 0], solver2.p[0], solver2.args[5].a0, solver2.args[5].c0, floquet_multiplier_threshold)
 
     for i in range(bounds.shape[0]):
 
@@ -240,7 +240,7 @@ def compute_LL(solver1, solver2, bounds, floquet_multiplier_threshold=0.8):
     
     return LL
 
-def continuation(solver, p_stop, step_size=1e-2, min_step_size=1e-4, max_step_size=1, maxiter=1000):
+def continuation(solver, p_stop, step_size=1e-2, min_step_size=1e-4, max_step_size=1, maxiter=1000, tol=1e-9):
 
     i = 0
     y_out = [solver.y]
@@ -266,7 +266,7 @@ def continuation(solver, p_stop, step_size=1e-2, min_step_size=1e-4, max_step_si
         i += 1
     
         try:
-            solver.solve()
+            solver.solve(atol=tol)
         except RuntimeError:
             solver.success = False
 
@@ -312,18 +312,18 @@ def fp_a(t, y, p, continuation_direction, y_prev, p_prev, ds, model, max_amplitu
                      (y - y_prev)@continuation_direction[:y.shape[0]] + (p - p_prev)@continuation_direction[y.shape[0]:] - ds])
 
 @jax.jit
-def f_rc(t, y, p, continuation_direction, y_prev, p_prev, ds, model, rc_direction, max_amplitude_species):
+def f_rc(t, y, p, continuation_direction, y_prev, p_prev, ds, model, reaction_consts, a0, rc_direction, max_amplitude_species):
     reaction_consts = np.exp(p[1] * rc_direction) * model.reaction_consts
-    return p[0] * model.f_red(t, y, reaction_consts=reaction_consts)
+    return p[0] * model.f_red(t, y, reaction_consts=reaction_consts, a0=a0)
 
 @jax.jit
-def fp_rc(t, y, p, continuation_direction, y_prev, p_prev, ds, model, rc_direction, max_amplitude_species):
+def fp_rc(t, y, p, continuation_direction, y_prev, p_prev, ds, model, reaction_consts, a0, rc_direction, max_amplitude_species):
     continuation_direction = continuation_direction / np.linalg.norm(continuation_direction)
     reaction_consts = np.exp(p[1] * rc_direction) * model.reaction_consts
-    return np.array([model.f_red(t[0], y[:KaiODE.n_dim - KaiODE.n_conserve], reaction_consts=reaction_consts)[max_amplitude_species],
+    return np.array([model.f_red(t[0], y[:KaiODE.n_dim - KaiODE.n_conserve], reaction_consts=reaction_consts, a0=a0)[max_amplitude_species],
                      (y - y_prev)@continuation_direction[:y.shape[0]] + (p - p_prev)@continuation_direction[y.shape[0]:] - ds])
 
-def sample(y0, period0, reaction_consts_0, bounds, step_size=1e-1, maxiter=1000, floquet_multiplier_threshold=7e-1, seed=None):
+def sample(y0, period0, reaction_consts_0, bounds, ds=1e-2, dt=1e-1, maxiter=1000, floquet_multiplier_threshold=7e-1, seed=None):
 
     a0 = 0.6
     a1 = 1.2
@@ -336,10 +336,10 @@ def sample(y0, period0, reaction_consts_0, bounds, step_size=1e-1, maxiter=1000,
 
     model = KaiODE(reaction_consts_0)
     p0_a = np.array([period0, a0])
-    ds = 1e-2
 
-    solvera = colloc(f_a, fp_a, y0, p0_a, args=(np.zeros(y0.size + np.size(period0) + 1).at[-1].set(1), y0.ravel(order="F"), p0_a, ds, model, max_amplitude_species))
+    initial_continuation_direction = np.zeros(y0.size + np.size(period0) + 1).at[-1].set(1)
 
+    solvera = colloc(f_a, fp_a, y0, p0_a, args=(initial_continuation_direction, y0.ravel(order="F"), p0_a, ds, model, max_amplitude_species))
     y_acont, p_acont = continuation(solvera, a1, step_size=ds)
 
     y_out = [[y0, y_acont[-1]]]
@@ -347,8 +347,10 @@ def sample(y0, period0, reaction_consts_0, bounds, step_size=1e-1, maxiter=1000,
     dperiod_out = []
     reaction_consts_out = [reaction_consts_0]
 
-    solver1 = colloc(f_rc, fp_rc, y0, np.array([period0]), args=(model, model.reaction_consts, a0, max_amplitude_species))
-    solver2 = colloc(f_rc, fp_rc, y_acont[-1], np.array([p_acont[-1, 0]]), args=(model, model.reaction_consts, p_acont[-1, 1], max_amplitude_species))
+    rc_direction = np.zeros_like(reaction_consts_0)
+
+    solver1 = colloc(f_rc, fp_rc, y0, np.array([period0, 0.]), args=(initial_continuation_direction, y0.ravel(order="F"), p0_rc, ds, model, reaction_consts_0, a0, rc_direction, max_amplitude_species))
+    solver2 = colloc(f_rc, fp_rc, y_acont[-1], np.array([p_acont[-1, 0], 0.]), args=(intial_continuation_direction, y0.ravel(order="F"), p0_rc, ds, model, reaction_consts_0, p_acont[-1, 1], rc_direction, max_amplitude_species))
     LL = compute_LL(solver1, solver2, bounds)
 
     #_, dp = compute_sensitivity_boundary(y0[:, 0], y0[:, -1], p0, reaction_consts_0, max_amplitude_species, M=M)
@@ -371,13 +373,8 @@ def sample(y0, period0, reaction_consts_0, bounds, step_size=1e-1, maxiter=1000,
         print("period: %.5f, %.5f"%(period_out[-1][0], period_out[-1][1]), flush=True)
         
         key, subkey = jax.random.split(key)
-        #u, s, vh = np.linalg.svd(dperiod_out[-1])
         randn = jax.random.normal(subkey, shape=(reaction_consts_0.shape[0],))
-        #s_full = np.zeros_like(reaction_consts_0).at[:s.shape[0]].set(s)
-        #sigma = step_size / (1 + s_full)
-        #step = (vh.T * (randn * sigma)).sum(axis=1)
-        #proposal_factor_f = -np.sum(randn**2) - np.pi * np.prod(sigma)
-        step = randn * step_size
+        step = randn * dt
         
         reaction_consts_propose = np.exp(np.log(reaction_consts_out[-1]) + step)
         max_amplitude_species = np.argmax(np.max(y_out[-1][0], axis=1) - np.min(y_out[-1][0], axis=1))
@@ -385,32 +382,24 @@ def sample(y0, period0, reaction_consts_0, bounds, step_size=1e-1, maxiter=1000,
         solver1.success = False
         solver1.y = y_out[-1][0]
         solver1.p = solver1.p.at[0].set(period_out[-1][0])
-        solver1.args = (model, reaction_consts_propose, a0, max_amplitude_species)
-        solver1.solve(atol=1e-9)
+        solver1.p = solver1.p.at[1].set(0)
+        solver1.args = (initial_continuation_direction, y_out[-1][0], period_out[-1][0], solver1.args[3], model, reaction_consts_out[-1], a0, step, max_amplitude_species)
+        continuation(solver1, p_stop=1, step_size=ds)
 
         solver2.success = False
         solver2.y = y_out[-1][1]
         solver2.p = solver2.p.at[0].set(period_out[-1][1])
-        solver2.args = (model, reaction_consts_propose, p_acont[-1, 1], max_amplitude_species)
-        solver2.solve(atol=1e-8)
+        solver2.args = (initial_continuation_direction, y_out[-1][1], period_out[-1][1], solver2.args[3], model, reaction_consts_out[-1], p_acont[-1, 1], step, max_amplitude_species)
+        continuation(solver2, p_stop=1, step_size=ds)
         
         if not (solver1.success and solver2.success):
             y_out.append(y_out[-1])
             period_out.append(period_out[-1])
             reaction_consts_out.append(reaction_consts_out[-1])
             failed += 1
-            #dperiod_out.append(dperiod_out[-1])
-            #print("Accept: %s"%(False))
             continue
             
         LL_propose = compute_LL(solver1, solver2)
-            
-        #_, dp_propose = compute_sensitivity_boundary(solver1.y[:, 0], solver1.y[:, -1], solver1.p[0], reaction_consts_propose, max_amplitude_species, M=M_propose)
-        #u, s, vh = np.linalg.svd(dp_propose)
-        #s_full = np.zeros_like(reaction_consts_0).at[:s.shape[0]].set(s)
-        #sigma = step_size / (1 + s_full)
-        #proposal_factor_r = -np.sum((-vh@step / sigma)**2) - np.pi * np.prod(sigma)
-        
         proposal_factor_r = 0
         proposal_factor_f = 0
 
@@ -418,15 +407,12 @@ def sample(y0, period0, reaction_consts_0, bounds, step_size=1e-1, maxiter=1000,
         
         key, subkey = jax.random.split(key)
         accept = np.log(jax.random.uniform(subkey)) < acceptance_ratio
-        #print("Accept: %s"%(accept))
-        #print("likelihood: %.5f"%(LL), "proposal likelihood: %.5f"%(LL_propose), "forward proposal factor: %.5f"%(proposal_factor_f), "reverse proposal factor: %.5f"%(proposal_factor_r))
         
         if accept:
             
             y_out.append([solver1.y, solver2.y])
             period_out.append([solver1.p[0], solver2.p[0]])
             reaction_consts_out.append(reaction_consts_propose)
-            #dperiod_out.append(dp_propose)
             LL = LL_propose
             accepted += 1
 
