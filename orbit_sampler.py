c18f7ed53e97b2c816afac5107ba478c3c0d0b95
Chris Chi <ccchi@midway3-login4.rcc.local>
2022-11-11 17:48:33 -0600

    energy and forces

--- orbit_sampler.py
+++ orbit_sampler.py
@@ -2,6 +2,7 @@ import numpy
 import diffrax
 import jax
 import jax.numpy as np
+import scipy.sparse
 from model import KaiODE
 from collocation import colloc
 import continuation
@@ -14,33 +15,83 @@ K = np.array(numpy.loadtxt(path + "/K.txt", dtype=numpy.int64))
 S = np.array(numpy.loadtxt(path + "/S.txt", dtype=numpy.int64))
 
 @jax.jit
-def LL_monodromy(y0, period, reaction_consts, a0=0.6, c0=3.5, floquet_multiplier_threshold=0.8):
+def E_monodromy(y0, period, reaction_consts, a0=0.6, c0=3.5, floquet_multiplier_threshold=0.8):
 
-    LL = 0
+    E = 0
 
     M = continuation.compute_monodromy_1(y0, period, reaction_consts, a0, c0)
     floquet_multipliers, evecs = np.linalg.eig(M)
     abs_multipliers = np.abs(floquet_multipliers)
-    LL += np.where(abs_multipliers > floquet_multiplier_threshold, 100 * (abs_multipliers - floquet_multipliers_threshold)**2, 0).sum()
+    E += np.where(abs_multipliers > floquet_multiplier_threshold, 100 * (abs_multipliers - floquet_multipliers_threshold)**2, 0).sum()
 
-def compute_LL(solver1, solver2, bounds, floquet_multiplier_threshold=0.8):
+def compute_energy_and_force(position, momentum, colloc_solver, bounds, floquet_multiplier_threshold=0.8):
 
-    LL = 0
-    LL -= 300 * (solver1.p[0] - 1)**2
-    LL -= 100 * (solver2.p[0] - 1)**2
-    LL -= 1 / np.linalg.norm(solver1.y[:, 1:] - solver1.y[:, :-1], axis=0).sum()**2
-    LL -= 1 / np.linalg.norm(solver2.y[:, 1:] - solver2.y[:, :-1], axis=0).sum()**2
-    LL += LL_monodromy(solver1.y[:, 0], solver1.p[0], solver1.args[5].a0, solver1.args[5].c0, floquet_multiplier_threshold)
-    LL += LL_monodromy(solver2.y[:, 0], solver2.p[0], solver2.args[5].a0, solver2.args[5].c0, floquet_multiplier_threshold)
+    E = 0
+    F = np.zeros(position)
+
+    J = colloc_solver.jac()
+    J_LU = scipy.sparse.linalg.splu(J) 
+    J = J[:-colloc_solver.n_par + 1, :-colloc_solver.n_par + 1]
+    J_rc = np.zeros((colloc_solver.n, position))
+
+    for i in range(position):
+
+        rc_direction = np.zeros(position).at[i].set(1)
+        args = list(colloc_solver.args)
+        args[-1] = rc_direction
+        colloc_solver.args = tuple(args)
+        dr_drc = jax.jit(jax.jacfwd(colloc_solver.resid, argnums=1))(colloc_solver.y, colloc_solver.p)[1, :-colloc_solver.n_par + 1]
+        J_rc = J_rc.at[i, :].set(J_LU.solve(numpy.asanyarray(dr_drc)))
+
+    E += 300 * (colloc_solver.p[0] - 1)**2
+    F -= 600 * J_rc[-colloc_solver.n_par, :]
+
+    arc_length = np.linalg.norm(colloc_solver.y[:, 1:] - colloc_solver.y[:, :-1], axis=0).sum()
+    d_arclength_dy = np.zeros(colloc_solver.y.shape)
+    d_arclength_dy = d_arclength_dy.at[:, 1:].add(colloc_solver.y[:, 1:] / np.linalg.norm(colloc_solver.y[:, 1:], axis=0))
+    d_arclength_dy = d_arclength_dy.at[:, :-1].add(colloc_solver.y[:, :-1] / np.linalg.norm(colloc_solver.y[:, :-1], axis=0))
+    E += 1 / arc_length**2
+    F -= (-2 / arc_length**4) * d_arclength_dy.ravel(order="F")@J_rc[:-colloc_solver.n_par, :]
+
+    E += E_monodromy(colloc_solver.y[:, 0], colloc_solver.p[0], colloc_solver.args[5].a0, colloc_solver.args[5].c0, floquet_multiplier_threshold)
+    F -= jax.jit(jax.jacfwd(E_monodromy), argnums=2)(colloc_solver.y[:, 0], colloc_solver.p[0], np.exp(position)) / np.exp(position)
 
     for i in range(bounds.shape[0]):
 
-        if solver1.args[0].reaction_consts[i] < bounds[i, 0]:
-            LL -= (np.log(bounds[i, 0]) - np.log(solver1.args[0].reaction_consts[i]))**2
-        elif solver1.args[0].reaction_consts[i] > bounds[i, 1]:
-            LL -= (np.log(bounds[i, 1]) - np.log(solver1.args[0].reaction_consts[i]))**2
+        if position[i] < bounds[i, 0]:
+            E += (bounds[i, 0] - position[i])**2
+            F -= F.at[i].add(2 * position[i])
+        elif position[i] > bounds[i, 1]:
+            E += (bounds[i, 1] - position[i])**2
+            F -= F.at[i].add(2 * position[i])
     
-    return LL
+    return E, F
+
+def termination_condition(colloc_solver):
+    return colloc_solver.p[-1] < 0
+
+def obabo(position, momentum, force_prev, dt, friction, prng_key, energy_function, energy_function_args):
+
+    prng_key, subkey = jax.random.split(key)
+
+    W = jax.random.normal(subkey, shape=momentum.shape)
+    c1 = np.exp(-dt * friction / 2)
+    momentum = c1 * momentum + c1 * W0 + (dt / 2) * force_prev
+    position = position + dt * momentum
+    
+    E, F = energy_function(position, momentum, *energy_function_args)
+
+    key, subkey = jax.random.split(prng_key)
+
+    W = jax.random.normal(subkey, shape=momentum.shape)
+    momentum = np.exp(-dt * friction / 2) * (momentum + (dt / 2) * force) + c1 * W
+
+    return position, momentum, E, F
+
+def generate_langevin_trajectory(position, dt, L, prng_key, stepper, energy_function, energy_function_args):
+
+    for i in range(L):
+        pass
 
 def sample(y0, period0, reaction_consts_0, bounds, ds=1e-2, dt=1e-1, maxiter=1000, floquet_multiplier_threshold=7e-1, seed=None):
 
@@ -68,9 +119,9 @@ def sample(y0, period0, reaction_consts_0, bounds, ds=1e-2, dt=1e-1, maxiter=100
 
     rc_direction = np.zeros_like(reaction_consts_0)
 
-    solver1 = colloc(f_rc, fp_rc, y0, np.array([period0, 0.]), args=(initial_continuation_direction, y0.ravel(order="F"), p0_rc, ds, model, reaction_consts_0, a0, rc_direction, max_amplitude_species))
+    colloc_solver = colloc(f_rc, fp_rc, y0, np.array([period0, 0.]), args=(initial_continuation_direction, y0.ravel(order="F"), p0_rc, ds, model, reaction_consts_0, a0, rc_direction, max_amplitude_species))
     solver2 = colloc(f_rc, fp_rc, y_acont[-1], np.array([p_acont[-1, 0], 0.]), args=(intial_continuation_direction, y0.ravel(order="F"), p0_rc, ds, model, reaction_consts_0, p_acont[-1, 1], rc_direction, max_amplitude_species))
-    LL = compute_LL(solver1, solver2, bounds)
+    LL = compute_LL(colloc_solver, solver2, bounds)
 
     #_, dp = compute_sensitivity_boundary(y0[:, 0], y0[:, -1], p0, reaction_consts_0, max_amplitude_species, M=M)
     #dp = dp * reaction_consts_0
@@ -98,12 +149,12 @@ def sample(y0, period0, reaction_consts_0, bounds, ds=1e-2, dt=1e-1, maxiter=100
         reaction_consts_propose = np.exp(np.log(reaction_consts_out[-1]) + step)
         max_amplitude_species = np.argmax(np.max(y_out[-1][0], axis=1) - np.min(y_out[-1][0], axis=1))
    
-        solver1.success = False
-        solver1.y = y_out[-1][0]
-        solver1.p = solver1.p.at[0].set(period_out[-1][0])
-        solver1.p = solver1.p.at[1].set(0)
-        solver1.args = (initial_continuation_direction, y_out[-1][0], period_out[-1][0], solver1.args[3], model, reaction_consts_out[-1], a0, step, max_amplitude_species)
-        cont(solver1, p_stop=1, step_size=ds)
+        colloc_solver.success = False
+        colloc_solver.y = y_out[-1][0]
+        colloc_solver.p = colloc_solver.p.at[0].set(period_out[-1][0])
+        colloc_solver.p = colloc_solver.p.at[1].set(0)
+        colloc_solver.args = (initial_continuation_direction, y_out[-1][0], period_out[-1][0], colloc_solver.args[3], model, reaction_consts_out[-1], a0, step, max_amplitude_species)
+        cont(colloc_solver, p_stop=1, step_size=ds)
 
         solver2.success = False
         solver2.y = y_out[-1][1]
@@ -111,14 +162,14 @@ def sample(y0, period0, reaction_consts_0, bounds, ds=1e-2, dt=1e-1, maxiter=100
         solver2.args = (initial_continuation_direction, y_out[-1][1], period_out[-1][1], solver2.args[3], model, reaction_consts_out[-1], p_acont[-1, 1], step, max_amplitude_species)
         cont(solver2, p_stop=1, step_size=ds)
         
-        if not (solver1.success and solver2.success):
+        if not (colloc_solver.success and solver2.success):
             y_out.append(y_out[-1])
             period_out.append(period_out[-1])
             reaction_consts_out.append(reaction_consts_out[-1])
             failed += 1
             continue
             
-        LL_propose = compute_LL(solver1, solver2)
+        LL_propose = compute_LL(colloc_solver, solver2)
         proposal_factor_r = 0
         proposal_factor_f = 0
 
@@ -129,8 +180,8 @@ def sample(y0, period0, reaction_consts_0, bounds, ds=1e-2, dt=1e-1, maxiter=100
         
         if accept:
             
-            y_out.append([solver1.y, solver2.y])
-            period_out.append([solver1.p[0], solver2.p[0]])
+            y_out.append([colloc_solver.y, solver2.y])
+            period_out.append([colloc_solver.p[0], solver2.p[0]])
             reaction_consts_out.append(reaction_consts_propose)
             LL = LL_propose
             accepted += 1
