fix energy+force calculation

continuation.py
@@ -147,17 +147,17 @@ def compute_monodromy(solver):
     return np.linalg.solve(-A1, A0)
 
 @jax.jit
-def compute_monodromy_1(y0, period, log_rc, a0=0.6, c0=3.5):
+def compute_monodromy_1(y0, period, reaction_consts, a0=0.6, c0=3.5):
 
     def rhs(t, y, args):
-        model = KaiODE(args[:-2])
-        return model.f_red(t, y, args[:model.reaction_consts.size], *args[-2:])
+        model = KaiODE(args[:reaction_consts.size])
+        return model.f_red(t, y, args[:model.reaction_consts.size], *args[reaction_consts.size:])
         
     term = diffrax.ODETerm(rhs)
     integrator = diffrax.Kvaerno4()
     stepsize_controller = diffrax.PIDController(rtol=1e-4, atol=1e-6)
         
-    return jax.jacfwd(diffrax.diffeqsolve, argnums=5)(term, integrator, 0, period, None, y0, args=np.concatenate([np.exp(log_rc), np.array([a0, c0])]), 
+    return jax.jacfwd(diffrax.diffeqsolve, argnums=5)(term, integrator, 0, period, None, y0, args=np.concatenate([reaction_consts, np.array([a0, c0])]), 
                                                       stepsize_controller=stepsize_controller, max_steps=10000).ys[-1]
 
 @partial(jax.jit, static_argnums=(2, 3))

orbit_sampler.py
@@ -20,9 +20,11 @@ def E_floquet(y0, period, log_rc, a0=0.6, c0=3.5, floquet_multiplier_threshold=0
     E = 0
 
     M = continuation.compute_monodromy_1(y0, period, np.exp(log_rc), a0, c0)
-    floquet_multipliers, evecs = np.linalg.eig(M)
+    floquet_multipliers = np.linalg.eigvals(M)
     abs_multipliers = np.abs(floquet_multipliers)
     E += np.where(abs_multipliers > floquet_multiplier_threshold, 100 * (abs_multipliers - floquet_multiplier_threshold)**2, 0).sum()
+
+    return E
 
 @jax.jit
 def grad_floquet(y0, period, log_rc, a0=0.6, c0=3.5, floquet_multiplier_threshold=0.8):
@@ -88,14 +90,14 @@ def compute_energy_and_force(position, momentum, colloc_solver, bounds, floquet_
         dr_drc = colloc_solver.jacp(colloc_solver.resid, argnums=1)(y_cont[-1].ravel(order="F"), p_cont[-1])[:-colloc_solver.n_par + 1, 1]
         J_rc = J_rc.at[i, :].set(J_LU.solve(numpy.asanyarray(dr_drc)))
 
-    E += 300 * (colloc_solver.p[0] - 1)**2
+    E += 300 * (p_cont[-1, 0] - 1)**2
     F -= 600 * J_rc[-colloc_solver.n_par, :]
 
-    E += E_arclength(colloc_solver.y)
-    F -= grad_arclength(colloc_solver.y).ravel(order="F")@J_rc[:-colloc_solver.n_par, :]
+    E += E_arclength(y_cont[-1])
+    F -= grad_arclength(y_cont[-1]).ravel(order="F")@J_rc[:-colloc_solver.n_par, :]
 
-    E += E_floquet(y_cont[-1, :, 0], p_cont[-1, 0], position, colloc_solver.args[5].c0, floquet_multiplier_threshold)
-    F -= grad_floquet(y_cont[-1, :, 0], p_cont[-1, 0], position, colloc_solver.args[5].c0, floquet_multiplier_threshold)
+    E += E_floquet(y_cont[-1, :, 0], p_cont[-1, 0], position, colloc_solver.args[5].a0, colloc_solver.args[5].c0, floquet_multiplier_threshold)
+    F -= grad_floquet(y_cont[-1, :, 0], p_cont[-1, 0], position, colloc_solver.args[5].a0, colloc_solver.args[5].c0, floquet_multiplier_threshold)
 
     for i in range(bounds.shape[0]):
 