8110f837f3d24f7a78af738eb6270def42434b5b
Chris Chi <ccchi@midway3-login4.rcc.local>
2023-03-04 19:56:49 -0600

    move newton solver to separate file

---
+++ nonlinear_solver.py
@@ -0,0 +1,70 @@
+import jax
+import jax.numpy as np
+from functools import partial
+jax.config.update("jax_enable_x64", True)
+
+@partial(jax.jit, static_argnums=1, 2, 3, 4)
+def newton(x, resid, jac=None, max_iter=20, tol=1e-9):
+
+    if jac is None:
+        jac = jax.jacfwd(resid)
+    
+    def cond(carry):
+        x, step, dx = carry
+        err = np.linalg.norm(dx)
+        return (step < max_iter) & (err > tol)
+    
+    def loop_body(carry):
+        x, step, dx = carry
+        dx = np.linalg.solve(jac(x), -resid(x))
+        return x + dx, step + 1, dx
+    
+    init = (x, 0, np.full_like(x, np.inf))
+    x, n_iter, dx = jax.lax.while_loop(cond, loop_body, init)
+    return x, np.linalg.norm(dx) < tol
+
+@partial(jax.jit, static_argnums=(1, 2, 3, 4, 5))
+def affine_covariant_newton(x, resid, jac=None, max_iter=20, min_damping_factor=1e-2, tol=1e-9):
+    
+    if jac is None:
+        jac = jax.jacfwd(resid)
+        
+    def loop_inner(carry):
+        x, x_guess, damping_factor, contraction_factor, trust_factor, dx_prev, dx_init, error, jac_lu = carry
+        x_guess = x + damping_factor * dx_init
+        error = jax.scipy.linalg.lu_solve(jac_lu, -resid(x_guess))
+        contraction_factor = np.linalg.norm(error) / np.linalg.norm(dx_init)
+        trust_factor = np.linalg.norm(dx_init) * damping_factor**2 / (2 * np.linalg.norm(error - (1 - damping_factor) * dx_init))
+        damping_factor = np.where(contraction_factor >= 1, np.minimum(trust_factor, damping_factor / 2), damping_factor)
+        damping_factor = np.where((contraction_factor < 1) * (trust_factor > 4 * damping_factor) * (trust_factor < 1), trust_factor, damping_factor)
+        return x, x_guess, damping_factor, contraction_factor, trust_factor, dx_prev, dx_init, error, jac_lu
+        
+    def cond_inner(carry):
+        x, x_guess, damping_factor, contraction_factor, trust_factor, dx_prev, dx_init, error, jac_lu = carry
+        return (damping_factor > min_damping_factor) & (np.linalg.norm(error) > tol) & ((trust_factor > 4 * damping_factor) * (trust_factor < 1) + (contraction_factor >= 1))
+        
+    def loop_outer(carry):
+        x, x_guess, step, damping_factor, contraction_factor, trust_factor, dx_prev, dx_init, error, jac_lu = carry
+        x, x_guess, damping_factor, contraction_factor, trust_factor, dx_prev, dx_init, error, jac_lu\
+            = jax.lax.while_loop(cond_inner, loop_inner, (x, x_guess, damping_factor, contraction_factor, trust_factor, dx_prev, dx_init, error, jac_lu))
+        
+        x = x_guess
+        jac_lu = jax.scipy.linalg.lu_factor(jac(x))
+        dx_prev = dx_init
+        dx_init = jax.scipy.linalg.lu_solve(jac_lu, -resid(x))
+        damping_factor = np.minimum(1, damping_factor * np.linalg.norm(dx_prev) * np.linalg.norm(error) / (np.linalg.norm((error - dx_init)) * np.linalg.norm(dx_init)))
+        contraction_factor = 1.
+        
+        return x, x + dx_init, step + 1, damping_factor, contraction_factor, trust_factor, dx_prev, dx_init, error, jac_lu
+        
+    def cond_outer(carry):
+        x, x_guess, step, damping_factor, contraction_factor, trust_factor, dx_prev, dx_init, error, jac_lu = carry
+        err = np.linalg.norm(dx_init)
+        return (step < max_iter) & (err > tol)
+    
+    jac_lu = jac_lu = jax.scipy.linalg.lu_factor(jac(x))
+    dx_init = jax.scipy.linalg.lu_solve(jac_lu, -resid(x))
+    init = (x, x, 0, 1., 1., 0., dx_init, dx_init, dx_init, jac_lu)
+    x, x_guess, step, damping_factor, contraction_factor, trust_factor, dx_prev, dx_init, error, jac_lu = jax.lax.while_loop(cond_outer, loop_outer, init)
+    
+    return x, step, np.linalg.norm(dx_init) < tol
