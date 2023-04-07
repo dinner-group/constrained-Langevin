928dafb5f005a3d4a60b3026af9fae7ddff4ac82
Chris Chi <ccchi@midway3-login3.rcc.local>
2023-04-07 11:23:47 -0500

    scale jacobian with diagonal

--- util.py
+++ util.py
@@ -31,20 +31,30 @@ def newton_polynomial(t, node_t, node_y, dd=None):
 
 class BVPJac:
 
-    def __init__(self, Jy, Jk, n_dim, n_par, n_mesh_intervals):
+    def __init__(self, Jy, Jk, n_dim, n_par, n_mesh_intervals, Jbc_left=None, Jbc_right=None):
         self.Jy = Jy
         self.Jk = Jk
         self.n_dim = n_dim
         self.n_par = n_par
         self.n_mesh_intervals = n_mesh_intervals
 
+        if Jbc_left is None:
+            self.Jbc_left = -np.identity(n_dim)
+        else:
+            self.Jbc_left = Jbc_left
+
+        if Jbc_right is None:
+            self.Jbc_right = np.identity(n_dim)
+        else:
+            self.Jbc_right = Jbc_right
+
     @jax.jit
     def todense(self):
 
         Jk = np.pad(np.vstack(self.Jk), ((0, self.n_dim), (0, 0)))
         Jy_dense = np.zeros((self.Jy.shape[0] * self.Jy.shape[1] + self.n_dim, self.Jy.shape[0] * self.Jy.shape[1] + self.n_dim))
-        Jy_dense = Jy_dense.at[-self.n_dim:, :self.n_dim].set(-np.identity(self.n_dim))
-        Jy_dense = Jy_dense.at[-self.n_dim:, -self.n_dim:].set(np.identity(self.n_dim))
+        Jy_dense = Jy_dense.at[-self.n_dim:, :self.n_dim].set(self.Jbc_left)
+        Jy_dense = Jy_dense.at[-self.n_dim:, -self.n_dim:].set(self.Jbc_right)
 
         def loop_body(carry, _):
             i, Jy_dense = carry
@@ -53,6 +63,25 @@ class BVPJac:
 
         Jy_dense = jax.lax.scan(loop_body, init=(0, Jy_dense), xs=None, length=self.Jy.shape[0])[0][1]
         return np.hstack([Jk[:, :self.n_par], Jy_dense, Jk[:, self.n_par:]])
+
+    @jax.jit
+    def right_multiply_diag(self, D):
+
+        Dy = D[self.n_par:self.n_par + self.Jy.shape[0] * self.Jy.shape[1] + self.n_dim]
+        Dk = np.concatenate([D[:self.n_par], D[self.n_par + self.Jy.shape[0] * self.Jy.shape[1] + self.n_dim:]])
+
+        def loop_body(carry, _):
+            i, Jy = carry
+            Dyi = jax.lax.dynamic_slice(Dy, (i * Jy.shape[1],), (Jy.shape[1] + self.n_dim,))
+            Jy = Jy.at[i].multiply(Dyi)
+            return (i + 1, Jy), _
+
+        Jy = jax.lax.scan(loop_body, init=(0, self.Jy), xs=None, length=self.Jy.shape[0])[0][1]
+        Jk = np.reshape(np.vstack(self.Jk) * Dk, (self.Jk.shape))
+        Jbc_left = self.Jbc_left * D[self.n_par:self.n_par + self.n_dim]
+        Jbc_right = self.Jbc_right * D[self.n_par + self.Jy.shape[0] * self.Jy.shape[1]:self.n_par + self.Jy.shape[0] * self.Jy.shape[1] + self.n_dim]
+        
+        return BVPJac(Jy, Jk, self.n_dim, self.n_par, self.n_mesh_intervals, Jbc_left, Jbc_right)
 
     @jax.jit
     def multiply_transpose(J1, J2):
@@ -76,21 +105,21 @@ class BVPJac:
         J1J2T = jax.lax.scan(loop2, init=(1, J1J2T), xs=None, length=J1.n_mesh_intervals - 2)[0][1]
         J1J2T = J1J2T.at[:Jy1Jy2T.shape[1], :2 * Jy1Jy2T.shape[1]].add(Jy1Jy2T[0, :, Jy1Jy2T.shape[1]:])
         J1J2T = J1J2T.at[(J1.n_mesh_intervals - 1) * Jy1Jy2T.shape[1]:J1.n_mesh_intervals * Jy1Jy2T.shape[1], (J1.n_mesh_intervals - 2) * Jy1Jy2T.shape[1]:J1.n_mesh_intervals * Jy1Jy2T.shape[1]].add(Jy1Jy2T[-1, :, :2 * Jy1Jy2T.shape[1]])
-        J1J2T = J1J2T.at[(np.diag_indices(J1.n_dim)[0] + J1J2T.shape[0] - 2, np.diag_indices(J1.n_dim)[0] + J1J2T.shape[0] - 2)].set(2)
-        J1J2T = J1J2T.at[:Jy1Jy2T.shape[1], -J1.n_dim:].set(-J1.Jy[0, :, :J1.n_dim])
-        J1J2T = J1J2T.at[-J1.n_dim:, :Jy1Jy2T.shape[1]].set(-J2.Jy[0, :, :J1.n_dim].T)
-        J1J2T = J1J2T.at[-Jy1Jy2T.shape[1] - J1.n_dim:-J1.n_dim, -J1.n_dim:].set(J1.Jy[-1, :, -J1.n_dim:])
-        J1J2T = J1J2T.at[-J1.n_dim:, -Jy1Jy2T.shape[1] - J1.n_dim:-J1.n_dim].set(J2.Jy[-1, :, -J2.n_dim:].T)
+        J1J2T = J1J2T.at[-J1.n_dim:, -J1.n_dim:].set(J1.Jbc_left@J2.Jbc_left + J1.Jbc_right@J2.Jbc_right)
+        J1J2T = J1J2T.at[:Jy1Jy2T.shape[1], -J1.n_dim:].set(J1.Jy[0, :, :J1.n_dim]@J2.Jbc_left)
+        J1J2T = J1J2T.at[-J1.n_dim:, :Jy1Jy2T.shape[1]].set(J1.Jbc_left@J2.Jy[0, :, :J1.n_dim].T)
+        J1J2T = J1J2T.at[-Jy1Jy2T.shape[1] - J1.n_dim:-J1.n_dim, -J1.n_dim:].set(J1.Jy[-1, :, -J1.n_dim:]@J2.Jbc_right)
+        J1J2T = J1J2T.at[-J1.n_dim:, -Jy1Jy2T.shape[1] - J1.n_dim:-J1.n_dim].set(J1.Jbc_right@J2.Jy[-1, :, -J2.n_dim:].T)
         
         return J1J2T
 
     def _tree_flatten(self):
-        children = (self.Jy, self.Jk)
+        children = (self.Jy, self.Jk, self.Jbc_left, self.Jbc_right)
         aux_data = {"n_dim":self.n_dim, "n_par":self.n_par, "n_mesh_intervals":self.n_mesh_intervals}
         return (children, aux_data)
 
     @classmethod
     def _tree_unflatten(cls, aux_data, children):
-        return cls(*children, **aux_data)
+        return cls(*children[:2], Jbc_left=children[2], Jbc_right=children[3], **aux_data)
 
 jax.tree_util.register_pytree_node(BVPJac, BVPJac._tree_flatten, BVPJac._tree_unflatten)
