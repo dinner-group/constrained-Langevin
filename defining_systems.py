b89d0b01e4e8fc3a120a3f22109fef3a71ef2fad
Chris Chi <ccchi@midway3-login4.rcc.local>
2023-04-24 14:03:55 -0500

    mesh density in likelihood

--- defining_systems.py
+++ defining_systems.py
@@ -143,12 +143,13 @@ def brusselator_bvp_potential(q, mesh_points=np.linspace(0, 1, 61)):
     
     n_points = (n_mesh_intervals * util.gauss_points.size + 1)
     y = q[model.Brusselator.n_par:model.Brusselator.n_par + n_points * model.Brusselator.n_dim].reshape(model.Brusselator.n_dim, n_points, order="F")
-    period = q[model.Brusselator.n_par + n_points * model.Brusselator.n_dim + 1]
     arclength = np.linalg.norm(y[:, 1:] - y[:, :-1], axis=0).sum()
     min_arclength = 0.3
+    _, mesh_density = util.recompute_mesh(y, mesh_points, util.gauss_points)
+    mesh_quality = (mesh_points.size - 1) * (mesh_density[1:] + mesh_density[:-1]) * (mesh_points[1:] - mesh_points[:-1]) / (2 * np.trapz(mesh_density, mesh_points))
     
+    E += 20 * (util.smooth_max(mesh_quality, smooth_max_temperature=6) - 1)**2
     E += 100 * np.where(np.abs(q[:model.Brusselator.n_par]) > np.log(100), (np.abs(q[:model.Brusselator.n_par]) - np.log(100))**2, 0).sum()
-    E += np.where(period > 12, 1 * (period - 12)**2, 0)
     E += np.where(arclength < min_arclength, (min_arclength / (np.sqrt(2) * arclength))**4 - (min_arclength / (np.sqrt(2) * arclength))**2 + 1 / 4, 0)
     
     return E
