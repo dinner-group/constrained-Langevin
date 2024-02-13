942c71fa2de0ba7559d32f7a4efbe616460864e8
Chris Chi <ccchi@midway3-login3.rcc.local>
2024-02-13 02:10:21 -0600

    typos

--- geodesic_langevin.py
+++ geodesic_langevin.py
@@ -256,7 +256,7 @@ def gBAOAB(position, momentum, lagrange_multiplier, energy=None, force=None, prn
     prng_key = prng_key.view(jax.random.PRNGKey(0).dtype)
     accept = True
     position_new, momentum_new, lagrange_multiplier_new, energy_new, force_new, J_and_factor_new\
-        = rattle_kick(position, momentum, energy, force, dt / 2, potential, constraint, jac_constraint, J_and_factor_new, linsol, *args, **kwargs)
+        = rattle_kick(position, momentum, energy, force, dt / 2, potential, constraint, jac_constraint, J_and_factor, linsol, *args, **kwargs)
     position_new, momentum_new, lagrange_multiplier_new, J_and_factor_new, success, n_iter\
         = rattle_drift(position_new, momentum_new, dt / 2, potential, constraint, jac_constraint, J_and_factor_new, linsol, nlsol, max_newton_iter, constraint_tol, reversibility_tol, *args, **kwargs)
     position_new, momentum_new, lagrange_multiplier_new, J_and_factor_new, prng_key = rattle_noise(position, momentum, prng_key, dt, friction, constraint, jac_constraint, J_and_factor, linsol, *args, **kwargs)
