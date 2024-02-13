0687b7ca032bc32646e55e489919d480c772e133
Chris Chi <ccchi@midway3-login3.rcc.local>
2024-02-13 02:09:49 -0600

    update production script for use with different bvp formulation

--- sample_repressilator_infer.py
+++ sample_repressilator_infer.py
@@ -65,6 +65,24 @@ def repressilator_log_bvp_mm_inference_potential(q, ode_model, *args, n_mesh_int
     E += (period - period_ref)**2  / (2 * std**2)
     return E
 
+@partial(jax.jit, static_argnames=("n_mesh_intervals"))
+def repressilator_log_bvp_mm_inference_potential_1(q, ode_model, colloc_points_unshifted=util.gauss_points_4, *args, n_mesh_intervals=60, **kwargs):
+
+    std = 5e-2
+    n_points = (n_mesh_intervals * colloc_points_unshifted.size + 1)
+    k = q[:ode_model.n_par]
+    y = q[ode_model.n_par:ode_model.n_par + n_points * ode_model.n_dim].reshape(ode_model.n_dim, n_points, order="F")
+    mesh_points = np.pad(np.cumsum(q[-n_mesh_intervals:]), (1, 0))
+    period = mesh_points[-1]
+    mesh_points = mesh_points / period
+    period_ref = t_eval[-1]
+    t = t_eval / t_eval[-1]
+    y_interp = util.interpolate(y, mesh_points, t)
+    E = repressilator_log_bvp_mm_potential(q, ode_model, *args, n_mesh_intervals)
+    E += jax.scipy.integrate.trapezoid((data_period_average - np.exp(y_interp[0]))**2 / (2 * std**2), x=t)
+    E += (period - period_ref)**2  / (2 * std**2)
+    return E
+
 @jax.jit
 def repressilator_log_bvp_inference_potential(q, ode_model, mesh_points=np.linspace(0, 1, 61)):
 
@@ -83,8 +101,7 @@ def repressilator_log_bvp_inference_potential(q, ode_model, mesh_points=np.linsp
     return E
 
 n_dim = argp.n_dim
-dt = 1e-2
-prng_key = np.load("repressilator_%d_lc_infer_key_%d_%d.npy"%(n_dim, argp.iter - 1, argp.process))[-1]
+dt = 1e-1
 friction = 1e-1
 
 n_points = n_mesh_intervals * colloc_points_unshifted.size + 1
@@ -93,19 +110,20 @@ rp = model.Repressilator_log_n(n_dim=n_dim)
 x = np.load("repressilator_%d_lc_infer_%d_%d.npy"%(n_dim, argp.iter - 1, argp.process))[-1]
 q0 = x[:rp.n_par + rp.n_dim * n_points + n_mesh_intervals]
 p0 = x[q0.size:2 * q0.size]
-args = (rp, colloc_points_unshifted)
+prng_key = x[-1:].view(np.uint32)
 
-potential = lambda *args:repressilator_log_bvp_mm_inference_potential(*args, n_mesh_intervals=n_mesh_intervals)
-resid = lambda *args:defining_systems.periodic_bvp_mm_colloc_resid(*args, n_mesh_intervals=n_mesh_intervals)
-jac = lambda *args:defining_systems.periodic_bvp_mm_colloc_jac(*args, n_mesh_intervals=n_mesh_intervals)
-n_constraints = resid(q0, *args).shape[0]
+potential = partial(repressilator_log_bvp_mm_inference_potential_1, n_mesh_intervals=n_mesh_intervals)
+resid = partial(defining_systems.bvp_mm_colloc_resid, n_mesh_intervals=n_mesh_intervals)
+jac = partial(defining_systems.bvp_mm_colloc_jac, n_mesh_intervals=n_mesh_intervals)
+n_constraints = resid(q0, ode_model=rp, colloc_points_unshifted=colloc_points_unshifted).shape[0]
 l0 = x[2 * q0.size:2 * q0.size + n_constraints]
 
 n_steps = argp.n_steps
 thin = argp.thin
 
-traj_rp_lc, key_lc = lgvn.gOBABO(q0, p0, l0, dt, friction, n_steps, thin, prng_key, potential, resid, jac, nlsol=nonlinear_solver.quasi_newton_bvp_symm_broyden, linsol=linear_solver.qr_ortho_proj_bvp,
-                                 max_newton_iter=100, tol=1e-9, args=args, metropolize=True, reversibility_tol=1e-6, print_acceptance=True)
+traj_rp_lc = lgvn.sample((q0, p0, l0, None, None, prng_key), dt=1e-1, n_steps=n_steps, thin=thin, friction=1e-1,
+                                 potential=potential, stepper=lgvn.gOBABO, constraint=resid, jac_constraint=jac, linsol=linear_solver.qr_ortho_proj_bvp, 
+                                 nlsol=nonlinear_solver.quasi_newton_bvp_symm_broyden, max_newton_iter=100, metropolize=True, reversibility_tol=1e-6, ode_model=rp, 
+                                 colloc_points_unshifted=util.gauss_points_4, print_acceptance=True)
 
 np.save("repressilator_%d_lc_infer_%d_%d.npy"%(n_dim, argp.iter, argp.process), traj_rp_lc)
-np.save("repressilator_%d_lc_infer_key_%d_%d.npy"%(n_dim, argp.iter, argp.process), key_lc)
